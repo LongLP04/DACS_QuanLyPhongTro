150ceea5636587bad248d2ff044666e00aa3c9c8
Bui Thi Tra Mi <trami27012004@gmail.com>
2025-12-11 14:29:01 +0700

    Chinh Sua giao dien

---
+++ Migrations/20251207080158_InitialIdentityModel.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DACS_QuanLyPhongTro.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialIdentityModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
